0dacf357492e9d26b1ec8a1ec48708d5c6eb5104
andyqee <vitasone@gmail.com>
2014-05-24 19:27:26 +0800

    Initial Commit

---
+++ SimpleWeather/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SimpleWeather
+//
+//  Created by andy on 24/5/14.
+//  Copyright (c) 2014 andy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SWAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SWAppDelegate class]));
+    }
+}
